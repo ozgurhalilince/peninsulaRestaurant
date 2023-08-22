9904a30cab8ceafd85ba6e2eba0bb84491430e21
ozgur <oince026@gmail.com>
2023-08-23 01:38:07 +0300

    login endpoint tests added

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -6,6 +6,11 @@ import User from "../src/models/User";
 import bcrypt from "bcryptjs";
 import mongoose from 'mongoose';
 
+beforeAll(done => {
+    mongoose.connection.dropDatabase()
+    done()
+})
+
 describe('auth tests', () => {
     it('should throw 401 if no authorization header', (done) => {
         request(server.callback())
@@ -139,6 +144,113 @@ describe('auth tests', () => {
                 if (err) return done(err);
                 return done();
             });
+    })
+
+    it('login - should return email is required', (done) => {
+        const payload = {
+            password: 'example',
+        }
+        request(server.callback())
+            .post('/api/v1/auth/login')
+            .send(payload)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400, apiMessages[1001])
+            .end(function(err, response) {
+                if (err) return done(err);
+                return done();
+            });
+    })
+
+    it('login - should return password is required', (done) => {
+        const payload = {
+            email: 'john-' + new Date().getTime() + '@peninsula.com',
+        }
+        request(server.callback())
+            .post('/api/v1/auth/login')
+            .send(payload)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400, apiMessages[1002])
+            .end(function(err, response) {
+                if (err) return done(err);
+                return done();
+            });
+    })
+
+    it('login - should return password is wrong when email not found on db', (done) => {
+        const payload = {
+            email: 'john-' + new Date().getTime() + '@peninsula.com',
+            password: 'example'
+        }
+        request(server.callback())
+            .post('/api/v1/auth/login')
+            .send(payload)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400, apiMessages[1020])
+            .end(function(err, response) {
+                if (err) return done(err);
+                return done();
+            });
+    })
+
+    it('login - should return password is wrong when request password is wrong', (done) => {
+        const userData = {
+            firstname: 'John',
+            lastname: 'Doe',
+            email: 'john-' + new Date().getTime() + '@peninsula.com',
+            password: bcrypt.hashSync('example', 5),
+            isDeleted: false,
+        };
+
+        User.create(userData).then(() => {
+            const payload = {
+                email: userData.email,
+                password: 'wrong-password',
+            }
+
+            request(server.callback())
+                .post('/api/v1/auth/login')
+                .send(payload)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(400, apiMessages[1020])
+                .end(function(err, response) {
+                    if (err) return done(err);
+                    return done();
+                });
+        });
+    })
+
+    it('login - should successfully login', (done) => {
+        const userData = {
+            firstname: 'John',
+            lastname: 'Doe',
+            email: 'john-' + new Date().getTime() + '@peninsula.com',
+            password: bcrypt.hashSync('example', 5),
+            isDeleted: false,
+        };
+        User.create(userData).then(() => {
+            const payload = {
+                email: userData.email,
+                password: 'example',
+            }
+
+            request(server.callback())
+                .post('/api/v1/auth/login')
+                .send(payload)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, response) {
+                    if (err) return done(err);
+
+                    assert.ok(typeof response.body.data.token !== "undefined")
+
+                    return done();
+                });
+        });
     })
 })
 
